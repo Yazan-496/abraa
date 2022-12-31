64cda3485c104833a221f255cf50cbd55877920e
Yazan Adanouf <yazan.adanouf@cloudypro.com>
2022-12-31 07:08:15 -0400

    Error handling class component

--- pages/errorHandling.js
+++ pages/errorHandling.js
@@ -1,6 +1,6 @@
-import react from "react"
+import React from "react"
 
-class ErrorBoundary extends Component {
+class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props)
 
